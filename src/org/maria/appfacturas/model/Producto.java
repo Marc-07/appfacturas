d85ec8bbe8e8c356295ead710f008defaa332f73
Marc7 <maanraca01@gmail.com>
2025-04-14 10:22:51 -0500

    constructor clase producto

--- src/org/maria/appfacturas/model/Producto.java
+++ src/org/maria/appfacturas/model/Producto.java
@@ -5,13 +5,14 @@ public class Producto {
     private int codigo;
     private String nombre;
     private float precio;
+    private static int ultimoCodigo;
+
+    public Producto() {
+        this.codigo = ++ultimoCodigo;
+    }
 
     public int getCodigo() {
         return codigo;
-    }
-
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
     }
 
     public String getNombre() {
