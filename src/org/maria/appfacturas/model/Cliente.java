abcb093908c1ead0d9355cc3efec1c97d84e2fe6
Marc7 <maanraca01@gmail.com>
2025-04-13 20:39:11 -0500

    Atributos, getter and setter de clase cliente

--- src/org/maria/appfacturas/model/Cliente.java
+++ src/org/maria/appfacturas/model/Cliente.java
@@ -1,4 +1,22 @@
 package org.maria.appfacturas.model;
 
 public class Cliente {
+    private String nombre;
+    private String nit;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
 }
