3f4ede706551aa6de4e08eb33064898cf1e7031f
Marc7 <maanraca01@gmail.com>
2025-04-14 11:18:18 -0500

    Metodo calcular importe

--- src/org/maria/appfacturas/model/ItemFactura.java
+++ src/org/maria/appfacturas/model/ItemFactura.java
@@ -24,4 +24,9 @@ public class ItemFactura {
     public void setProducto(Producto producto) {
         this.producto = producto;
     }
+
+    //Metodos
+    public float calcularImporte(){
+        return  this.cantidad * this.producto.getPrecio();
+    }
 }
