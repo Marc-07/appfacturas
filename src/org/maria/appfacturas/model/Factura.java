66444e62df5ae60e56d845e4328a40a32005bcd8
Marc7 <maanraca01@gmail.com>
2025-04-13 20:43:34 -0500

    Atributos, getter and setter de clase Factura

--- src/org/maria/appfacturas/model/Factura.java
+++ src/org/maria/appfacturas/model/Factura.java
@@ -1,4 +1,34 @@
 package org.maria.appfacturas.model;
 
+import java.util.Date;
+
 public class Factura {
+
+    private int folio;
+    private String descripcion;
+    private Date fecha;
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
 }
