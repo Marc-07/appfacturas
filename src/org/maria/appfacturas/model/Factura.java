90759b2c6229f6ccd80f3a2e24cbff86a5bd366a
Marc7 <maanraca01@gmail.com>
2025-04-14 11:36:11 -0500

    Generar detalles de la factura uso de StringBuilder

--- src/org/maria/appfacturas/model/Factura.java
+++ src/org/maria/appfacturas/model/Factura.java
@@ -1,5 +1,6 @@
 package org.maria.appfacturas.model;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Factura {
@@ -83,6 +84,29 @@ public class Factura {
                 .append("\n")
                 .append("\n#\tNombre\t$\tCant.\tTotal\n");
 
+        SimpleDateFormat df = new SimpleDateFormat("dd 'de' MMMM, yyyy");
+        sb.append("Fecha Emisión: ")
+                .append(df.format(this.fecha))
+                .append("\n");
+
+        for (ItemFactura item: this.items){
+            if(item == null){
+                continue;
+            }
+            sb.append(item.getProducto().getCodigo())
+                    .append("\t")
+                    .append(item.getProducto().getNombre())
+                    .append("\t")
+                    .append(item.getProducto().getPrecio())
+                    .append("\t")
+                    .append(item.getCantidad())
+                    .append("\t")
+                    .append(item.calcularImporte())
+                    .append("\n");
+
+        }
+        sb.append("\nTotal a pagar: ")
+                .append(calcularTotal());
         return sb.toString();
     }
 }
