4216124e3ece56a47e113e7a72e46bb2409ace27
Marc7 <maanraca01@gmail.com>
2025-04-14 10:25:56 -0500

    Relacionar clase factura con clase cliente

--- src/org/maria/appfacturas/model/Factura.java
+++ src/org/maria/appfacturas/model/Factura.java
@@ -7,13 +7,23 @@ public class Factura {
     private int folio;
     private String descripcion;
     private Date fecha;
+    private Cliente cliente;
+    private ItemFactura[] items;
+    private int indiceItems;
+    public static final int MAX_ITEMS = 10;
+    private static int ultimoFolio;
+
+    //Constructor
+    public Factura(String descripcion, Cliente cliente) {
+        this.descripcion = descripcion;
+        this.cliente = cliente;
+        this.items = new ItemFactura[MAX_ITEMS];
+        this.folio = ++ultimoFolio;
+        this.fecha = new Date();
+    }
 
     public int getFolio() {
         return folio;
-    }
-
-    public void setFolio(int folio) {
-        this.folio = folio;
     }
 
     public String getDescripcion() {
@@ -30,5 +40,23 @@ public class Factura {
 
     public void setFecha(Date fecha) {
         this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ItemFactura[] getItems() {
+        return items;
+    }
+
+    public void addItemFactura(ItemFactura item){
+        if (indiceItems < MAX_ITEMS){
+            this.items[indiceItems++] = item;
+        }
     }
 }
